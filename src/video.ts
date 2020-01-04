f0ca370832c9338c0b5da8fbd771a82388a7fd78
Filip Bech-Larsen <filipbech@filipbech.dk>
2020-01-04 17:38:16 +0100

    more videos

--- src/video.ts
+++ src/video.ts
@@ -1,4 +1,5 @@
-export const video = {
+export const videos = [
+{
     "video":"bEfCKGBJc6k",
     "title":"Cadence Training With GCN | Over/Under Interval Workout On The Passo Sella",
     "startTime":2,
@@ -103,4 +104,835 @@ export const video = {
         "cadence":90,
         "duration":60
     }]
+},{
+    "video":"z2maxEqA2g8",
+    "title":"Power Pyramids on the Passo Pordoi | Indoor Training With GCN",
+    "startTime":13,
+    "sections": [{
+        "name":"warmup",
+        "effort":2,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"warmup",
+        "effort":3,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"warmup",
+        "effort":5,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"Tempo",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"Sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"Sprint",
+        "effort":10,
+        "cadence":70,
+        "duration":30
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"tempo",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sprint",
+        "effort":10,
+        "cadence":70,
+        "duration":30
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"tempo",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sprint",
+        "effort":10,
+        "cadence":70,
+        "duration":30
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"tempo",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sweetspot",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"sprint",
+        "effort":10,
+        "cadence":70,
+        "duration":30
+    },{
+        "name":"cooldown",
+        "effort":5,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"cooldown",
+        "effort":3,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"cooldown",
+        "effort":2,
+        "cadence":90,
+        "duration":60
+    }]
+},{
+    "video":"5-PtoExp6bU",
+    "title":"HIIT Indoor Cycling Workout | 40 Minute Muscular Endurance Intervals",
+    "startTime":4,
+    "sections": [{
+        "name":"warmup",
+        "effort":4,
+        "cadence":100,
+        "duration":600
+    },{
+        "name":"torque",
+        "effort":10,
+        "cadence":70,
+        "duration":300
+    },{
+        "name":"cadence",
+        "effort":9,
+        "cadence":100,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":10,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"out of saddle",
+        "effort":10,
+        "cadence":55,
+        "duration":120
+    },{
+        "name":"recovery",
+        "effort":6,
+        "cadence":80,
+        "duration":240
+    },{
+        "name":"torque",
+        "effort":10,
+        "cadence":70,
+        "duration":300
+    },{
+        "name":"cadence",
+        "effort":9,
+        "cadence":100,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":10,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"out of saddle",
+        "effort":10,
+        "cadence":55,
+        "duration":120
+    },{
+        "name":"cooldown",
+        "effort":6,
+        "cadence":80,
+        "duration":120
+    }]
+},{
+    "video":"ZiGE3-L4vyg",
+    "title":"HIIT Indoor Cycling Workout | 30 Minute Intervals: Fitness Training",
+    "startTime":1,
+    "sections": [{
+        "name":"warmup",
+        "effort":5,
+        "cadence":70,
+        "duration":300
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":150
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":150
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":120
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":120
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":90
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"rest",
+        "effort":1,
+        "cadence":70,
+        "duration":90
+    },{
+        "name":"max effort",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"cooldown",
+        "effort":1,
+        "cadence":70,
+        "duration":360
+    }]
+},{
+    "video":"68NzLRMIhpI",
+    "title":"Quick HIIT Indoor Cycling Workout | 22 Minute Strength Intervals",
+    "startTime":9,
+    "sections": [{
+        "name":"warmup",
+        "effort":5,
+        "cadence":75,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":60,
+        "duration":60
+    },{
+        "name":"rev",
+        "effort":10,
+        "cadence":110,
+        "duration":60
+    },{
+        "name":"recovery",
+        "effort":3,
+        "cadence":85,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":60,
+        "duration":60
+    },{
+        "name":"rev",
+        "effort":10,
+        "cadence":110,
+        "duration":60
+    },{
+        "name":"recovery",
+        "effort":3,
+        "cadence":85,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":60,
+        "duration":60
+    },{
+        "name":"rev",
+        "effort":10,
+        "cadence":110,
+        "duration":60
+    },{
+        "name":"recovery",
+        "effort":3,
+        "cadence":85,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":60,
+        "duration":60
+    },{
+        "name":"rev",
+        "effort":10,
+        "cadence":110,
+        "duration":60
+    },{
+        "name":"recovery",
+        "effort":3,
+        "cadence":85,
+        "duration":120
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":60,
+        "duration":60
+    },{
+        "name":"rev",
+        "effort":10,
+        "cadence":110,
+        "duration":60
+    },{
+        "name":"warm down",
+        "effort":1,
+        "cadence":75,
+        "duration":120
+    }]
+},{
+    "video":"J9fAk04oTtA",
+    "title":"HIIT Indoor Cycling Workout | 45 Minute VO2 Max Intervals",
+    "startTime":5,
+    "sections": [{
+        "name":"warmup",
+        "effort":2,
+        "cadence":100,
+        "duration":300
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"torque",
+        "effort":9,
+        "cadence":80,
+        "duration":30
+    },{
+        "name":"attacking",
+        "effort":10,
+        "cadence":60,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":5,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"tempo",
+        "effort":7,
+        "cadence":100,
+        "duration":300
+    },{
+        "name":"warm down",
+        "effort":2,
+        "cadence":90,
+        "duration":300
+    }]
+},{
+    "video":"SZRk3OY2RAY",
+    "title":"Quick Progressive Effort 20 Minute Workout | Climb The Passo Gardena With GCN",
+    "startTime":12,
+    "sections": [{
+        "name":"warmup",
+        "effort":1,
+        "cadence":70,
+        "duration":90
+    },{
+        "name":"ramp 1",
+        "effort":1,
+        "cadence":80,
+        "duration":90
+    },{
+        "name":"ramp 2",
+        "effort":2,
+        "cadence":85,
+        "duration":90
+    },{
+        "name":"ramp 3",
+        "effort":3,
+        "cadence":85,
+        "duration":90
+    },{
+        "name":"ramp 4",
+        "effort":4,
+        "cadence":90,
+        "duration":90
+    },{
+        "name":"ramp 5",
+        "effort":5,
+        "cadence":90,
+        "duration":90
+    },{
+        "name":"ramp 6",
+        "effort":6,
+        "cadence":90,
+        "duration":90
+    },{
+        "name":"ramp 7",
+        "effort":7,
+        "cadence":95,
+        "duration":90
+    },{
+        "name":"ramp 8",
+        "effort":8,
+        "cadence":95,
+        "duration":90
+    },{
+        "name":"ramp 9",
+        "effort":9,
+        "cadence":95,
+        "duration":90
+    },{
+        "name":"ramp 10",
+        "effort":10,
+        "cadence":95,
+        "duration":90
+    },{
+        "name":"ramp 11",
+        "effort":10,
+        "cadence":100,
+        "duration":90
+    },{
+        "name":"warm down",
+        "effort":1,
+        "cadence":70,
+        "duration":120
+    }]
+},{
+    "video":"gryPYiipR5M",
+    "title":"HIIT Indoor Cycling Workout | 25 Minute Sprint Intervals",
+    "startTime":3,
+    "sections": [{
+        "name":"warmup",
+        "effort":5,
+        "cadence":80,
+        "duration":420
+    },{
+        "name":"max sprint",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":2,
+        "cadence":80,
+        "duration":180
+    },{
+        "name":"max sprint",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":2,
+        "cadence":80,
+        "duration":180
+    },{
+        "name":"max sprint",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":2,
+        "cadence":80,
+        "duration":180
+    },{
+        "name":"max sprint",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"recovery",
+        "effort":2,
+        "cadence":80,
+        "duration":180
+    },{
+        "name":"max sprint",
+        "effort":10,
+        "cadence":100,
+        "duration":30
+    },{
+        "name":"warm down",
+        "effort":5,
+        "cadence":80,
+        "duration":240
+    }]
+},{
+    "video":"IfR1l3a3uNA",
+    "title":"HIIT Indoor Cycling Workout | 55 Minute FTP Threshold Intervals",
+    "startTime":2,
+    "sections": [{
+        "name":"warmup",
+        "effort":4,
+        "cadence":85,
+        "duration":420
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"Recovery",
+        "effort":5,
+        "cadence":85,
+        "duration":300,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"zone 4",
+        "effort":8,
+        "cadence":90,
+        "duration":60
+    },{
+        "name":"zone 3",
+        "effort":7,
+        "cadence":95,
+        "duration":240,
+    },{
+        "name":"cool down",
+        "effort":3,
+        "cadence":85,
+        "duration":240
+    }]
+},{
+    "video":"0iwLDaovcCk",
+    "title":"Train With Mitchelton-Scott | Improve Your Threshold Power",
+    "startTime":0,
+    "sections": [{
+        "name":"warmup",
+        "effort":2,
+        "cadence":90,
+        "duration":60
+    }, {
+        "name":"warmup",
+        "effort":4,
+        "cadence":90,
+        "duration":120
+    },{
+        "name":"warmup",
+        "effort":4,
+        "cadence":90,
+        "duration":120
+    },{
+        "name":"",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":9,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":8,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"",
+        "effort":9,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"",
+        "effort":7,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":8,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":9,
+        "cadence":90,
+        "duration":180
+    },{
+        "name":"",
+        "effort":8,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"",
+        "effort":9,
+        "cadence":70,
+        "duration":180
+    },{
+        "name":"cool down",
+        "effort":5,
+        "cadence":90,
+        "duration":120
+    },{
+        "name":"cool down",
+        "effort":4,
+        "cadence":90,
+        "duration":120
+    },{
+        "name":"cool down",
+        "effort":2,
+        "cadence":90,
+        "duration":60
+    }]
 }
+];
